08bb000275911036c7ecbbf777f532f9edab654f
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-06 04:31:31 -0300

    Se agrego login y register.

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home/index.html"), name="index"),
     path("about/", TemplateView.as_view(template_name="home/about.html"), name="about"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", LogoutView.as_view(template_name="home/logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from . import forms
 
-# Create your views here.
+# Login basado en funciones
+def login_request(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = forms.CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, "home/index.html", {"mensaje": f"Se ha logueado correctamente."})
+    else:
+        form = AuthenticationForm()
+    return render(request, "home/login.html", {"form": form})
+
+
+# Registro basado en funciones
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = forms.CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request, "home/index.html", {"mensaje": "Usuario creado correctamente."})
+    else:
+        form = forms.CustomUserCreationForm()
+    return render(request, "home/register.html", {"form": form})
+
