aca34196864277f11bf874c3fd6d6b240e0bc726
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-06-06 03:49:56 -0300

    Se crea el proyecto y app home

---
+++ project/apps/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="home/index.html"), name="index"),
+    path("about/", TemplateView.as_view(template_name="home/about.html"), name="about"),
+]
